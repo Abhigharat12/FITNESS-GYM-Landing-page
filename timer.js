46ab3a68f6e6c2a715715fff30fab5327d4cf415
Abhishek Gharat <156587972+Abhigharat12@users.noreply.github.com>
2025-02-07 18:43:27 +0530

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,21 +1,27 @@
 
-var countDownDate = new Date().getTime() + (24 * 60 * 60 * 1000);
+    document.addEventListener("DOMContentLoaded", function () {
+        var countDownDate = new Date().getTime() + (24 * 60 * 60 * 1000);
 
-var x = setInterval(function() {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
+        var x = setInterval(function() {
+            var now = new Date().getTime();
+            var distance = countDownDate - now;
 
-  
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); // Added semicolon here
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  
-  document.getElementById("demo").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+            var demoElement = document.getElementById("demo");
+            if (demoElement) {
+                demoElement.innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+            }
 
-  
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
+            if (distance < 0) {
+                clearInterval(x);
+                if (demoElement) {
+                    demoElement.innerHTML = "EXPIRED";
+                }
+            }
+        }, 1000);
+    });
+
+ss
